25a7c117c19c493b05d77e630dc375fb967e3d70
Nick <nmarks99@gmail.com>
2022-08-04 16:50:34 -0500

    saveg

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include<Arduino.h>
+#include "usart.h"
 
 void setup() {
-
+  usart_init();
 }
 
 void loop() {
-
+  delay(1000);
+  char m[50];
+  int years = 22;
+  sprintf(m,"Hello I am %d years old",years);
+  usart_println(m);
 }
---
+++ test/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+
+
+    // char x[10];
+    // int len = sizeof(x)/sizeof(char);
+    // printf("%d\n",len);
+    return 0;
+
+}
