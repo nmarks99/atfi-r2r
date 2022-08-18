8bbee2430d88d333a1bb483e3669215f84d35b14
Nick <nmarks99@gmail.com>
2022-08-18 15:23:24 -0500

    Save

--- save.sh
+++ save.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-echo 'git add --all && git commit -m"Save" && git push\n'
+echo 'git add --all && git commit -m"Save" && git push'
 git add --all && git commit -m"Save" && git push
 
